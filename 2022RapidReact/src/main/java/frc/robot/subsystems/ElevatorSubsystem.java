741b561c45a26fd5096d1d018c54df2a892bd98f
isstark <88864013+isstark@users.noreply.github.com>
2022-02-12 09:02:26 -0600

    2/12/22 switch test 1

--- 2022RapidReact/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ 2022RapidReact/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,89 +1,78 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-//import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+// import com.ctre.phoenix.motorcontrol.can.BaseTalonConfiguration;
 //import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.DigitalInput;
+// import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
 
   private TalonSRX outRightElevator;
-  private TalonSRX outLeftElevator;
+  // private TalonSRX outLeftElevator;
 
-  private TalonSRX inRightElevator;
-  private TalonSRX inLeftElevator;
-
-  // private boolean switchAngleExtend = false;
-  // private boolean switchAngleRetrack = false;
-  // private boolean switchVerticalExtend = false;
-  // private boolean switchVerticalRetrack = false;
-
-  DigitalInput switchAngleExtend = new DigitalInput(1);
-  DigitalInput switchAngleRetrack = new DigitalInput(2);
-  DigitalInput switchVerticalExtend = new DigitalInput(3);
-  DigitalInput switchVerticalRetrack = new DigitalInput(4);
+  // private TalonSRX inRightElevator;
+  // private TalonSRX inLeftElevator;
 
   public ElevatorSubsystem() {
 
     outRightElevator = new TalonSRX(6);
-    outLeftElevator = new TalonSRX(7);
+    outRightElevator.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector,LimitSwitchNormal.NormallyOpen);
+    // outLeftElevator = new TalonSRX(7);
 
-    inRightElevator = new TalonSRX(8);
-    inLeftElevator = new TalonSRX(9);
+    // inRightElevator = new TalonSRX(8);
+    // inLeftElevator = new TalonSRX(9);
 
     outRightElevator.setInverted(false);
-    outLeftElevator.setInverted(false);
+    // outLeftElevator.setInverted(false);
 
-    inRightElevator.setInverted(false);
-    inLeftElevator.setInverted(false);
+    // inRightElevator.setInverted(false);
+    // inLeftElevator.setInverted(false);
   }
 
   public void startAngleElevator(double speed) {
+    outRightElevator.set(ControlMode.PercentOutput, speed);
+    // outLeftElevator.set(ControlMode.PercentOutput, speed);
 
-    if (switchAngleExtend.get()) {
-      inRightElevator.set(ControlMode.PercentOutput, 0.0);
-
-    } else {
-      outRightElevator.set(ControlMode.PercentOutput, speed);
-      outLeftElevator.set(ControlMode.PercentOutput, speed);
-    }
   }
 
+  // help.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector,
+  // LimitSwitchNormal.NormallyOpen);
   public void startVerticalElevator(double speed) {
 
-    if (switchVerticalExtend.get()) {
-      inRightElevator.set(ControlMode.PercentOutput, 0.0);
+    // if (switchVerticalExtend) {
+    //   inRightElevator.set(ControlMode.PercentOutput, 0.0);
 
-    } else {
-      inRightElevator.set(ControlMode.PercentOutput, speed);
-      inLeftElevator.set(ControlMode.PercentOutput, speed);
-    }
+    // } else {
+    //   inRightElevator.set(ControlMode.PercentOutput, speed);
+    //   inLeftElevator.set(ControlMode.PercentOutput, speed);
+    // }
   }
-
 
   public void retrackAngleElevator(double speed) {
 
-    if (switchAngleRetrack.get()) {
-      inRightElevator.set(ControlMode.PercentOutput, 0.0);
+    // if (switchAngleRetrack.get()) {
+    //   inRightElevator.set(ControlMode.PercentOutput, 0.0);
 
-    } else {
-      outRightElevator.set(ControlMode.PercentOutput, speed);
-      outLeftElevator.set(ControlMode.PercentOutput, speed);
-    }
+    // } else {
+    //   outRightElevator.set(ControlMode.PercentOutput, speed);
+    //   outLeftElevator.set(ControlMode.PercentOutput, speed);
+    // }
   }
 
   public void retrackVerticalElevator(double speed) {
 
-    if (switchVerticalRetrack.get()) {
-      inRightElevator.set(ControlMode.PercentOutput, 0.0);
+    // if (switchVerticalRetrack.get()) {
+    //   inRightElevator.set(ControlMode.PercentOutput, 0.0);
 
-    } else {
-      inRightElevator.set(ControlMode.PercentOutput, speed);
-      inLeftElevator.set(ControlMode.PercentOutput, speed);
-    }
+    // } else {
+    //   inRightElevator.set(ControlMode.PercentOutput, speed);
+    //   inLeftElevator.set(ControlMode.PercentOutput, speed);
+    // }
   }
 
   @Override
