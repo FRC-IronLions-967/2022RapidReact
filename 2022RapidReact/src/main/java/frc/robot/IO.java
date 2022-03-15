a7aa49839e39f7a6eb45a0a781644b7c94b060c0
isstark <88864013+isstark@users.noreply.github.com>
2022-03-15 09:31:04 -0500

    Update IO.java

--- 2022RapidReact/src/main/java/frc/robot/IO.java
+++ 2022RapidReact/src/main/java/frc/robot/IO.java
@@ -30,8 +30,8 @@ public class IO {
         manipulatorController.whenButtonReleased("B", new StartRollerCommand(0.0));
         manipulatorController.whenButtonPressed("Y", new RunShooterCommand(-1.0));
         manipulatorController.whenButtonReleased("Y", new RunShooterCommand(0.0));
-        manipulatorController.whenButtonPressed("LBUMP", new ChangeRPMCommand(0.4));
-        manipulatorController.whenButtonPressed("RBUMP", new ChangeRPMCommand(0.65));
+        manipulatorController.whenButtonPressed("LBUMP", new ChangeRPMCommand(0.37));
+        manipulatorController.whenButtonPressed("RBUMP", new ChangeRPMCommand(0.75));
         manipulatorController.whenButtonPressed("START", new RunFlyWheelCommand(true));
         manipulatorController.whenButtonPressed("SELECT", new RunFlyWheelCommand(false));
         manipulatorController.whenPOVButtonPressed("N", new RunArmWinchCommand(0.6));
